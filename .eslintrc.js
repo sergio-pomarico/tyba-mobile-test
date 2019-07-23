6d1516bb7a6fea3e87faf6149555d2f300d8d9b1
Sergio Pomarico <sergiodavid21@gmail.com>
2019-07-23 16:20:32 -0500

    add git hooks, eslint, editorconfig and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,31 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    "@react-native-community",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended"
+  ],
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  env: {
+    "browser": true,
+    "jasmine": true,
+    "jest": true
+  },
+  rules: {
+    "prettier/prettier": ["error", { "singleQuote": true }],
+    "@typescript-eslint/explicit-member-accessibility": 0,
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-use-before-define": 0,
+    "react/prop-types": 0
+  },
+  settings: {
+    "react": {
+      "pragma": "React",
+      "version": "detect"
+    }
+  },
+  parser: "@typescript-eslint/parser"
 };
